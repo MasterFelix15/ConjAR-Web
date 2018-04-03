1df8a445ef34c074a4ebdeff8bda9a8aa81aaffd
MasterFelix <felixatnus2013@gmail.com>
2018-04-03 12:21:51 +0800

    Fix double click issue
    set a cooldown time between each button click

--- public/javascripts/selectable.js
+++ public/javascripts/selectable.js
@@ -5,39 +5,63 @@ AFRAME.registerComponent('selectable', {
     },
 
     init: function () {
+        this.handleSelect = this.handleSelect.bind(this);
+    },
+
+    play: function () {
         var data = this.data;
-        var el = this.el;
+        this.el.addEventListener(data.on, this.handleSelect);
+    },
 
-        el.addEventListener(data.on, function (evt) {
-            var markerEl = document.querySelector('#marker');
-            var menuEl = document.querySelector('#menu');
-            var sceneEl = document.querySelector('a-scene');
+    pause: function () {
+        var data = this.data;
+        this.el.addEventListener(data.on, this.handleSelect);
+    },
 
-            if (markerEl.getAttribute('visible') === false) {return;}
-            markerEl.setAttribute('visible', "false");
-            menuEl.setAttribute('visible', "false");
+    handleSelect: function (evt) {
+        if (document.querySelector('.button').getAttribute('class') === "button locked") {return;}
+        this.lockButtons();
+        var data = this.data;
+        var markerEl = document.querySelector('#marker');
+        var menuEl = document.querySelector('#menu');
+        var sceneEl = document.querySelector('a-scene');
 
-            var matrix = new THREE.Matrix4();
-            matrix.extractRotation( markerEl.object3D.matrix );
+        if (markerEl.getAttribute('visible') === false) {return;}
+        markerEl.setAttribute('visible', "false");
+        menuEl.setAttribute('visible', "false");
 
-            var direction = new THREE.Vector3( 0, 0, 1 );
-            direction = matrix.multiplyVector3( direction );
+        var matrix = new THREE.Matrix4();
+        matrix.extractRotation( markerEl.object3D.matrix );
 
-            var scale=0.2;
+        var direction = new THREE.Vector3( 0, 0, 1 );
+        direction = matrix.multiplyVector3( direction );
 
-            var entityEl = document.querySelector("#" + data.src + "-prototype");
+        var scale=0.2;
 
-            var clone = entityEl.cloneNode(true);
-            sceneEl.appendChild(clone);
+        var entityEl = document.querySelector("#" + data.src + "-prototype");
 
-            var calculated_position = new THREE.Vector3().addVectors(markerEl.object3D.position, direction.clone().normalize().multiplyScalar(scale));
-            entityEl.setAttribute('position', calculated_position);
-            entityEl.setAttribute('visible', true);
+        var clone = entityEl.cloneNode(true);
+        sceneEl.appendChild(clone);
+
+        var calculated_position = new THREE.Vector3().addVectors(markerEl.object3D.position, direction.clone().normalize().multiplyScalar(scale));
+        entityEl.setAttribute('position', calculated_position);
+        entityEl.setAttribute('visible', true);
 
 
-            var lookAtTarget = new THREE.Vector3().addVectors(calculated_position, direction);
-            entityEl.object3D.lookAt(lookAtTarget);
-            entityEl.removeAttribute('id');
-        });
+        var lookAtTarget = new THREE.Vector3().addVectors(calculated_position, direction);
+        entityEl.object3D.lookAt(lookAtTarget);
+        entityEl.removeAttribute('id');
+    },
+
+    lockButtons: function () {
+        var btnEls = document.querySelectorAll('.button');
+        for (var i = 0; i < btnEls.length; i++) {
+            btnEls[i].setAttribute('class', "button locked");
+        }
+        setTimeout(function () {
+            for (var i = 0; i < btnEls.length; i++) {
+                btnEls[i].setAttribute('class', "button");
+            }
+        }, 500);
     }
 });
